d42b70f4cc108efa403887a304998902d59a9977
Pranav Karawale <pranav@karawale.in>
2022-06-08 18:54:18 +0000

    Host SSH keys for use in GitHub Codespaces

--- api/v1.go
+++ api/v1.go
@@ -22,5 +22,15 @@ func V1Handler(w http.ResponseWriter, r *http.Request) {
 		ctx.String(http.StatusOK, os.Getenv("PUB_GPG_KEY"))
 	})
 
+	sshGroup := v1.Group("/ssh")
+	sshGroup.Use(middleware.GitHubMiddleware)
+
+	sshGroup.GET("/gh_codespaces.id_ed25519", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, os.Getenv("PRIV_SSH_KEY"))
+	})
+	sshGroup.GET("/gh_codespaces.id_ed25519.pub", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, os.Getenv("PUB_SSH_KEY"))
+	})
+
 	router.ServeHTTP(w, r)
 }
